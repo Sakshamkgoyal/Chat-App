cfae065fc0819f1704565e96db029bf558c918bf
Saksham Goyal <sakshamgoyal278@gmail.com>
2023-09-08 19:14:36 +0530

    register complete

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,16 +1,76 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import Logo from "../assets/logo.svg"
+import Logo from "../assets/logo.svg";
+import {ToastContainer, toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
+import axios from 'axios';
+import { registerRoute } from "../utils/APIRoutes";
 
 function Register() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert("form");
+  const navigate = useNavigate();
+  const [values,setValues] = useState({
+    username: "",
+    email:"",
+    password:"", 
+    confirmPassword:"",
+  });
+
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 80000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
   };
 
-  const handleChange = (event) => { }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (handleValidation()) {
+      const { email, username, password } = values;
+      const { data } = await axios.post(registerRoute, {
+        username,
+        email,
+        password,
+      });
+
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem(
+          process.env.REACT_APP_LOCALHOST_KEY,
+          JSON.stringify(data.user)
+        );
+        navigate("/");
+      }
+    }
+  };
+
+  const handleChange = (event) => { 
+    setValues({...values, [event.target.name]:event.target.value})
+  }
+
+  const handleValidation = () =>{
+    const {password, confirmPassword, username, email} = values;
+    if(password!== confirmPassword){
+      toast.error("password and confirm password should be same", toastOptions );
+      return false;
+    } else if(username.length<3){
+      toast.error("username should be greater than three chracter", toastOptions );
+      return false;
+    } else if(password.length<8){
+      toast.error("Password should be equal or greater than 8 characters", toastOptions );
+      return false;
+    }else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
+    }
+    return true;
+  }
+
   return (
+    <>
     <FormContainer>
       <form onSubmit={(event) => handleSubmit(event)}>
         <div className="brand">
@@ -25,6 +85,8 @@ function Register() {
         <span>Already have an account ? <Link to={"/login"}>Login</Link></span>
       </form>
     </FormContainer>
+    <ToastContainer/>
+    </>
   )
 }
 
